7b61276e0ffdb0e3b4cf1096073948345a53989d
greninjam26 <94926874+greninjam26@users.noreply.github.com>
2026-06-18 21:22:14 -0400

    Implemented Loading and Not Found pages

---
+++ app/loading.tsx
@@ -0,0 +1,12 @@
+function LoadingPage() {
+	return (
+		<div className="wrapper flex min-h-[50vh] flex-col items-center justify-center gap-4 text-center">
+			<div className="size-16 animate-spin rounded-full border-6 border-muted border-t-primary" />
+			<p className="text-sm text-muted-foreground">
+				Loading Pokemon Store...
+			</p>
+		</div>
+	);
+}
+
+export default LoadingPage;
