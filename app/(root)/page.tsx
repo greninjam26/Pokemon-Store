680423f74266df997831db12b3f9df6dfa24de40
greninjam26 <94926874+greninjam26@users.noreply.github.com>
2026-06-21 21:34:18 -0400

    Reinforce database requestions

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,8 @@
 import ProductList from "@/components/shared/product/product-list";
 import { getLatestProducts } from "@/lib/action/product.action";
 
+export const dynamic = "force-dynamic";
+
 async function Homepage() {
 	const latestProducts = await getLatestProducts();
 
