7d44f31fa62f15516a9c9c417d4babcb2f96d0ab
greninjam26 <94926874+greninjam26@users.noreply.github.com>
2026-06-30 14:25:21 -0400

    Added db tables

---
+++ prisma/migrations/20260630182328_add_user_based_tables/migration.sql
@@ -0,0 +1,12 @@
+/*
+  Warnings:
+
+  - Made the column `name` on table `User` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "password" TEXT,
+ADD COLUMN     "paymentMethod" TEXT,
+ADD COLUMN     "role" TEXT NOT NULL DEFAULT 'user',
+ALTER COLUMN "name" SET NOT NULL,
+ALTER COLUMN "name" SET DEFAULT 'NO_NAME';
