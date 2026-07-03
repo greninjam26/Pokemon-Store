04b0d73765713939e3421399fdf5eec87dcf89b4
greninjam26 <94926874+greninjam26@users.noreply.github.com>
2026-07-02 20:18:39 -0400

    Implemented Sign up action

--- lib/action/user.actions.ts
+++ lib/action/user.actions.ts
@@ -1,9 +1,11 @@
 "use server";
 
+import { hashSync } from "bcrypt-ts-edge";
 import { isRedirectError } from "next/dist/client/components/redirect-error";
 
 import { signIn, signOut } from "@/auth";
-import { signInFormSchema } from "@/lib/validator";
+import prisma from "@/db/prisma";
+import { signInFormSchema, signUpFormSchema } from "@/lib/validator";
 
 type ActionResponse = {
 	success: boolean;
@@ -45,4 +47,60 @@ export async function signInWithCredentials(
 
 export async function signOutUser() {
 	await signOut({ redirectTo: "/" });
+}
+
+export async function signUpUser(
+	_previousState: unknown,
+	formData: FormData,
+): Promise<ActionResponse> {
+	try {
+		const user = signUpFormSchema.parse({
+			name: formData.get("name"),
+			email: formData.get("email"),
+			password: formData.get("password"),
+			confirmPassword: formData.get("confirmPassword"),
+			callbackUrl: formData.get("callbackUrl") || "/",
+		});
+
+		const existingUser = await prisma.user.findUnique({
+			where: {
+				email: user.email,
+			},
+		});
+
+		if (existingUser) {
+			return {
+				success: false,
+				message: "User already exists",
+			};
+		}
+
+		await prisma.user.create({
+			data: {
+				name: user.name,
+				email: user.email,
+				password: hashSync(user.password, 10),
+			},
+		});
+
+		await signIn("credentials", {
+			email: user.email,
+			password: user.password,
+			redirectTo: user.callbackUrl,
+		});
+
+		return {
+			success: true,
+			message: "User registered successfully",
+		};
+	} catch (error) {
+		if (isRedirectError(error)) {
+			throw error;
+		}
+
+		return {
+			success: false,
+			message: "Could not register user",
+		};
+	}
 }
--- lib/validator.ts
+++ lib/validator.ts
@@ -47,3 +47,24 @@ export const signInFormSchema = z.object({
 });
 
 export type SignInForm = z.infer<typeof signInFormSchema>;
+
+export const signUpFormSchema = z
+	.object({
+		name: z.string().trim().min(3, "Name must be at least 3 characters"),
+		email: z.email("Invalid email address"),
+		password: z
+			.string()
+			.trim()
+			.min(6, "Password must be at least 6 characters"),
+		confirmPassword: z
+			.string()
+			.trim()
+			.min(6, "Confirm password must be at least 6 characters"),
+		callbackUrl: z.string().trim().default("/"),
+	})
+	.refine((data) => data.password === data.confirmPassword, {
+		message: "Passwords do not match",
+		path: ["confirmPassword"],
+	});
+
+export type SignUpForm = z.infer<typeof signUpFormSchema>;
